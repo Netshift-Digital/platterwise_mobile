6ed461fcc585be0f04f904a5cc5bf3231ef017f5
user <chisometi@gmail.com>
2022-09-22 09:37:19 +0100

    first commit

--- lib/views/screens/auth/login.dart
+++ lib/views/screens/auth/login.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:platterwave/res/color.dart';
 import 'package:platterwave/res/spacing.dart';
 import 'package:platterwave/res/text-theme.dart';
 import 'package:platterwave/res/theme.dart';
 import 'package:platterwave/utils/nav.dart';
-import 'package:platterwave/views/screens/auth/login.dart';
 import 'package:platterwave/views/screens/auth/otp.dart';
 import 'package:platterwave/views/widget/appbar/appbar.dart';
 import 'package:platterwave/views/widget/button/custom-button.dart';
@@ -12,15 +12,15 @@ import 'package:platterwave/views/widget/text_feild/country_field.dart';
 import 'package:platterwave/views/widget/text_feild/text_feild.dart';
 import 'package:the_validator/the_validator.dart';
 
-class Register extends StatelessWidget {
-   Register({Key? key}) : super(key: key);
-    final TextEditingController _fullName =  TextEditingController();
-   final TextEditingController _email =  TextEditingController();
-   final TextEditingController _username =  TextEditingController();
-   final TextEditingController _password =  TextEditingController();
-   final TextEditingController _phoneNumber =  TextEditingController();
+class Login extends StatelessWidget {
+  Login({Key? key}) : super(key: key);
+
+  final TextEditingController _email =  TextEditingController();
+  final TextEditingController _password =  TextEditingController();
+
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
     return GestureDetector(
       onTap: (){
         FocusScope.of(context).unfocus();
@@ -28,7 +28,7 @@ class Register extends StatelessWidget {
       child: AnnotatedRegion(
         value: kOverlay,
         child: Scaffold(
-         // appBar: appBar(context),
+          // appBar: appBar(context),
           body: SingleChildScrollView(
             physics:const BouncingScrollPhysics(),
             child: SafeArea(
@@ -37,34 +37,18 @@ class Register extends StatelessWidget {
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                   const SizedBox(height: twenty,),
-                     Text("Create your account",style: AppTextTheme.h4.copyWith(
-                       fontSize: twentyFour,
-                       fontWeight: FontWeight.w700
-                     ),),
-                    const SizedBox(height: fiftyFour,),
-                    Text("Full name",style: AppTextTheme.hint),
-                    const SizedBox(height: hintSpacing,),
-                    Field(
-                        controller: _fullName,
-                        hint: "John Doe",
-                        validate: FieldValidator.minLength(3),
-                    ),
                     const SizedBox(height: twenty,),
+                    Text("Sign in",style: AppTextTheme.h4.copyWith(
+                        fontSize: twentyFour,
+                        fontWeight: FontWeight.w700
+                    ),),
+                    const SizedBox(height: fiftyFour,),
                     Text("Email",style: AppTextTheme.hint),
                     const SizedBox(height: hintSpacing,),
                     Field(
                       controller: _email,
                       hint: "gsswuw@gmail.com",
                       validate: FieldValidator.email(),
-                    ),
-                    const SizedBox(height: twenty,),
-                    Text("Username",style: AppTextTheme.hint),
-                    const SizedBox(height: hintSpacing,),
-                    Field(
-                      controller: _username,
-                      hint: "Johndone12",
-                      validate: FieldValidator.minLength(3),
                     ),
                     const SizedBox(height: twenty,),
                     Text("Password",style: AppTextTheme.hint),
@@ -75,50 +59,70 @@ class Register extends StatelessWidget {
                       isPassword: true,
                       validate: FieldValidator.minLength(3),
                     ),
-                    const SizedBox(height: twenty,),
-                    Text("Phone number",style: AppTextTheme.hint),
-                    const SizedBox(height: hintSpacing,),
-                    CountryField(
-                      controller: _phoneNumber,
-                      hint: "55-0114-2346",
-                      validate: FieldValidator.minLength(3),
-                    ),
-                    const SizedBox(height: forty,),
+                   const SizedBox(height:four,),
+                   Row(
+                     mainAxisAlignment: MainAxisAlignment.end,
+                     children: [
+                       Text("Forgot password",style: AppTextTheme.h4.copyWith(
+                         fontSize: 14,
+                         fontWeight: FontWeight.w500
+                       ),)
+                     ],
+                   ),
+                   SizedBox(height: size.height*0.07,),
                     PlatButton(
                         title: "Create your account",
                         onTap: (){
-                            nav(context,Otp());
+                          nav(context,Otp());
                         }
                     ),
-                    const SizedBox(height: eighteen,),
+                    SizedBox(height: size.height*0.05,),
+                   Row(
+                     mainAxisAlignment: MainAxisAlignment.center,
+                     children: [
+                       SvgPicture.asset("assest/icon/login with.svg"),
+                     ],
+                   ),
+                    SizedBox(height: size.height*0.02,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        const SizedBox(width: 1,),
+                        SvgPicture.asset("assest/icon/apple.svg"),
+                        SvgPicture.asset("assest/icon/facebook.svg"),
+                        Image.asset("assest/icon/google.png"),
+                        const SizedBox(width: 1,),
+                      ],
+                    ),
+                    SizedBox(height: size.height*0.02,),
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
                         Row(),
                         Text("we wont share your information without your permission",
                             style: AppTextTheme.hint.copyWith(
-                              fontSize: 12,
-                              fontWeight: FontWeight.w500,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
                                 color: AppColor.g500
                             )
                         ),
-                        const SizedBox(height: hintSpacing,),
+                        SizedBox(height: size.height*0.04,),
                         GestureDetector(
                           onTap: (){
-                            nav(context, Login());
+                            Navigator.pop(context);
                           },
                           child: RichText(
                             text: TextSpan(
                               children: [
                                 TextSpan(
-                                  text: 'Already have an account? ',
+                                  text: 'No account yet? ',
                                   style: AppTextTheme.h6.copyWith(
                                       fontSize: 14,
-                                    color: AppColor.g500
+                                      color: AppColor.g500
                                   ),
                                 ),
                                 TextSpan(
-                                  text: 'Sign in',
+                                  text: 'Create an account',
                                   style: AppTextTheme.h6.copyWith(
                                       color:AppColor.p200,
                                       fontSize: 14
