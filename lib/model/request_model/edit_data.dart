3f40b2e23b928dc0ddc88e9acfe658c9615cac18
John-Maricool <ebukaikenwa0902@gmail.com>
2023-09-25 22:51:42 -1100

    Edit profile endpoint added

--- lib/model/request_model/edit_data.dart
+++ lib/model/request_model/edit_data.dart
@@ -3,15 +3,21 @@ class EditData {
   String bio;
   String location;
   String profileURL;
+  String fullName;
+  String username;
+  String number;
+  String email;
 
-  EditData({
-   required this.firebaseAuthID,
-   required this.bio,
-    required this.location,
-    this.profileURL ='https://thumbs.dreamstime.com/b/lonely-elephant-against-sunset-beautiful-sun-clouds-savannah-serengeti-national-park-africa-tanzania-artistic-imag-image-106950644.jpg'
-  });
-
-
+  EditData(
+      {required this.firebaseAuthID,
+      required this.bio,
+      required this.location,
+      required this.fullName,
+      required this.username,
+      required this.number,
+      required this.email,
+      this.profileURL =
+          'https://thumbs.dreamstime.com/b/lonely-elephant-against-sunset-beautiful-sun-clouds-savannah-serengeti-national-park-africa-tanzania-artistic-imag-image-106950644.jpg'});
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
@@ -19,6 +25,10 @@ class EditData {
     data['bio'] = this.bio;
     data['location'] = this.location;
     data['profileURL'] = this.profileURL;
+    data['phone'] = this.number;
+    data['email'] = this.email;
+    data['username'] = this.username;
+    data['full_name'] = this.fullName;
     return data;
   }
 }
